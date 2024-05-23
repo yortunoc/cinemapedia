324bf5b81c415a96f974cf92acff4e735a7f1ba9
yortuno <yortuno@buk.cl>
2024-05-22 22:52:13 -0400

    first commit

---
+++ lib/config/route/app_router.dart
@@ -0,0 +1,10 @@
+import 'package:cinemapedia/presentation/screens/screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    name: HomeScreen.name,
+    builder: (context, state) => const HomeScreen(),
+  )
+]);
